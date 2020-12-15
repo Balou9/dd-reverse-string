8257f9296ca444566d64dc0cb8264a49bfd827f6
Balou9 <emmanuel.owosekun@gmail.com>
2020-12-15 11:52:02 +0100

    ci: adds terraform api token

--- main.tf
+++ main.tf
@@ -131,3 +131,23 @@ resource "aws_iam_role_policy_attachment" "attach_reverseStringHandlerPolicy" {
   role       = aws_iam_role.role.name
   policy_arn = aws_iam_policy.policy.arn
 }
+
+# resource "aws_lambda_function" "lambda" {
+#   filename      = "lambda_function_payload.zip"
+#   function_name = "lambda_function_name"
+#   role          = aws_iam_role.iam_for_lambda.arn
+#   handler       = "exports.test"
+#
+#   # # The filebase64sha256() function is available in Terraform 0.11.12 and later
+#   # # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+#   # # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+#   # source_code_hash = filebase64sha256("lambda_function_payload.zip")
+#
+#   runtime = "nodejs12.x"
+#
+#   environment {
+#     variables = {
+#
+#     }
+#   }
+# }
