e1da4c35bd1c6aa107e68f66559cdb97255c9a02
Balou9 <emmanuel.owosekun@gmail.com>
2020-12-18 10:40:23 +0100

    feat: added bucket

--- main.tf
+++ main.tf
@@ -13,12 +13,19 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-// Declare variables set by github actions workflow
 
-variable "stack_name" {
+// Declare variables set by github actions workflow
+variable "reverse_string" {
 
   type = string
-  description = "The stack-name"
+  description = "The string 'reverse-string' to use for resource naming of revelvant resources"
+
+}
+
+variable "string" {
+
+  type = string
+  description = "The string 'string' to use for resource naming of revelvant resources"
 
 }
 
@@ -27,16 +34,57 @@ resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
   comment = "dd-reverse-strings-oai"
 }
 
-// Bucket
+// string Bucket
+resource "aws_s3_bucket" "string_bucket" {
+  tags = {
+    Key   = "dd-string:name"
+    Value = "dd-string"
+  }
+  bucket = "${var.string}-bucket"
+}
+
+// reverse_string Bucket
 resource "aws_s3_bucket" "reverse_string_bucket" {
   tags = {
     Key   = "dd-reverse-string:name"
     Value = "dd-reverse-string"
   }
-  bucket = "${var.stack_name}-bucket"
+  bucket = "${var.reverse_string}-bucket"
+}
+// String Bucket Policy
+data "aws_iam_policy_document" "string_bucket_policy_document" {
+
+  statement {
+    sid    = "AllowOriginAccesIdentity"
+    effect = "allow"
+    principals {
+      type        = "CanonicalUser"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.s3_canonical_user_id]
+    }
+    actions = [
+      "s3:Get*",
+      "s3:List*"
+    ]
+    resources = [
+      aws_s3_bucket.string_bucket.arn,
+      "arn:aws:s3:::${aws_s3_bucket.string_bucket.bucket}/*"
+    ]
+  }
+
+  statement {
+    sid    = "AllowReverseStringHandlerGetObject"
+    effect = "allow"
+    principals {
+      type        = "AWS"
+      identifiers = [aws_lambda_function.reverse_string_handler.arn]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.string_bucket.bucket}/*"]
+  }
+
 }
 
-// Bucket Policy
+// reverse_string Bucket Policy
 data "aws_iam_policy_document" "reverse_string_bucket_policy_document" {
 
   statement {
@@ -54,17 +102,6 @@ data "aws_iam_policy_document" "reverse_string_bucket_policy_document" {
       aws_s3_bucket.reverse_string_bucket.arn,
       "arn:aws:s3:::${aws_s3_bucket.reverse_string_bucket.bucket}/*"
     ]
-  }
-
-  statement {
-    sid    = "AllowReverseStringHandlerGetObject"
-    effect = "allow"
-    principals {
-      type        = "AWS"
-      identifiers = [aws_lambda_function.reverse_string_handler.arn]
-    }
-    actions   = ["s3:GetObject"]
-    resources = ["arn:aws:s3:::${aws_s3_bucket.reverse_string_bucket.bucket}/*"]
   }
 
   statement {
@@ -91,31 +128,18 @@ data "aws_iam_policy_document" "reverse_string_bucket_policy_document" {
 
 }
 
-resource "aws_s3_bucket_policy" "policy" {
+resource "aws_s3_bucket_policy" "string_policy" {
+  bucket = aws_s3_bucket.string_bucket.id
+  policy = data.aws_iam_policy_document.string_bucket_policy_document.id
+}
+
+resource "aws_s3_bucket_policy" "reverse_string_policy" {
   bucket = aws_s3_bucket.reverse_string_bucket.id
   policy = data.aws_iam_policy_document.reverse_string_bucket_policy_document.id
 }
 
 
-data "aws_iam_policy_document" "assume_role_policy_document" {
-  statement {
-    sid    = "AllowAssumeRoleByLambda"
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
-
 // LambdaExecutionRole
-resource "aws_iam_role" "role" {
-  name               = "ReverseStringHandlerExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy_document.json
-}
-
-
 data "aws_iam_policy_document" "reverse_string_handler_execution_policy" {
 
   statement {
@@ -133,7 +157,7 @@ data "aws_iam_policy_document" "reverse_string_handler_execution_policy" {
     sid       = "AllowS3GetObject"
     effect    = "Allow"
     actions   = ["s3:GetObject"]
-    resources = ["arn:aws:s3:::${aws_s3_bucket.reverse_string_bucket.bucket}/*"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.string_bucket.bucket}/*"]
   }
 
   statement {
@@ -151,6 +175,24 @@ data "aws_iam_policy_document" "reverse_string_handler_execution_policy" {
   }
 
 }
+
+data "aws_iam_policy_document" "assume_role_policy_document" {
+  statement {
+    sid    = "AllowAssumeRoleByLambda"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name               = "ReverseStringHandlerExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy_document.json
+}
+
 // LambdaExecutionPolicy
 resource "aws_iam_policy" "policy" {
   name   = "ReverseStringHandlerExecutionPolicy"
@@ -162,9 +204,10 @@ resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
   policy_arn = aws_iam_policy.policy.arn
 }
 
+// ReverseStringHandler
 resource "aws_lambda_function" "reverse_string_handler" {
   filename      = "dummy.zip"
-  function_name = "${var.stack_name}-handler"
+  function_name = "${var.reverse_string}-handler"
   role          = aws_iam_role.role.arn
   handler       = "exports.handler"
   runtime       = "nodejs12.x"
