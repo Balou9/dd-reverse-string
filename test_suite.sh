9b196718c2e7fd5a6540da2baa34066112e11ceb
Balou9 <emmanuel.owosekun@gmail.com>
2020-12-22 11:25:04 +0100

    test: assert status

--- test_suite.sh
+++ test_suite.sh
@@ -1,11 +1,13 @@
 test_reverse_string_200() {
   printf "test_reverse_string_200\n"
-  resp_head="$(mktemp)"
+  status="$(mktemp)"
+  string="$(mktemp)"
   resp_body="$(mktemp)"
 
   aws lambda invoke --function-name reverse-string-handler $resp_body
 
-  cat $resp_body | jq .statusCode
-  cat $resp_body | jq .Body
+  cat $resp_body | jq .statusCode >> $status
+  cat $resp_body | jq .body >> $string
 
+  assert_status $status 200
 }
