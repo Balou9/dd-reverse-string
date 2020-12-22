343c532a13af2035cf99fe91b590736cc96d44ae
Balou9 <emmanuel.owosekun@gmail.com>
2020-12-22 15:29:36 +0100

    test: assign status to variable

--- test_suite.sh
+++ test_suite.sh
@@ -1,10 +1,8 @@
 test_reverse_string_200() {
   printf "test_reverse_string_200\n"
-  status="$(mktemp)"
   resp_body="$(mktemp)"
 
   aws lambda invoke --function-name reverse-string-handler $resp_body
-
-  cat $resp_body | jq .statusCode >> $status
+  status=$(cat $resp_body | jq .statusCode)
   assert_equal $status 200
 }
