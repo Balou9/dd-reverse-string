c34d68481cba2d3f1b4d27de8eacc5370bc23ae4
Balou9 <emmanuel.owosekun@gmail.com>
2020-12-22 15:02:20 +0100

    feat: put reversed string to destination bucket

--- lambdas/string/reverse/index.js
+++ lambdas/string/reverse/index.js
@@ -1,47 +1,23 @@
 const { S3 } = require("aws-sdk");
-const s3 = new S3({
+const string_bucket_s3 = new S3({
   apiVersion: "2006-03-01"
 })
 
 module.exports.handler = async function handler (event, context) {
-
   try {
-
-    const payload = await s3.getObject({
+    var payload = await s3.getObject({
       Key: "string.json",
       Bucket: process.env.STRING_BUCKET_NAME
     }).promise()
-
-
-
-    return {
-
-      "statusCode": 200,
-      "headers": {
-        "content-type": "application/json",
-      },
-      "body": JSON.stringify(payload)
-
-    }
-
+    var reversed_string = payload.body.toString().split("").reverse().join("")
+    await s3.putObject({
+      Key: "reversed_string.json",
+      Bucket: process.env.REVERSE_STRING_BUCKET_NAME,
+      Body: reversed_string
+    }).promise()
+    
+    return { "statusCode": 200 }
   } catch (err) {
-
-    if ( err.code === "NoSuchKey" ) {
-      return { "statusCode": 404 }
-    }
-
-    return {
-      "statusCode": err.code,
-      "body": err.message
-    }
-
+    return { "statusCode": 500 }
   }
-}
-
-if (!process.env.STRING_BUCKET_NAME) {
-  throw new Error("missing required env var STRING_BUCKET_NAME");
-}
-
-if (!process.env.REVERSE_STRING_BUCKET_NAME) {
-  throw new Error("missing required env var REVERSE_STRING_BUCKET_NAME");
 }
--- test_suite.sh
+++ test_suite.sh
@@ -7,7 +7,5 @@ test_reverse_string_200() {
   aws lambda invoke --function-name reverse-string-handler $resp_body
 
   cat $resp_body | jq .statusCode >> $status
-  cat $resp_body | jq .body >> $string
-
-  assert_status $status 200
+  assert_equal $status 200
 }
